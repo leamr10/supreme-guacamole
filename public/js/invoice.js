9fe22d060a2116ef60a542ca49db384194bddc73
jbarz <jbarz0415@gmail.com>
2023-07-19 11:59:42 -0400

    invoice and total price

--- public/js/invoice.js
+++ public/js/invoice.js
@@ -1,11 +1,14 @@
 const menuItems = JSON.parse (localStorage.getItem("menu-item") || []);
-
+let totalPriceEl = document.getElementById('total-price');
+let totalPrice = 0;
 for (let i=0; i < menuItems.length; i++) {
     const newRow = document.createElement("tr");
     newRow.innerHTML = 
     `<td>${menuItems[i].name}</td>
      <td>${menuItems[i].price}</td>
     `;
-
+totalPrice = menuItems[i].price++;
 document.querySelector('#invoice-table').appendChild(newRow);
 }
+console.log(totalPrice);
+totalPriceEl.textContent = totalPrice;
